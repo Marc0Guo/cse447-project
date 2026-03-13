9fb2c4b9205d3aea8bcf1a1b73cd122d83c5892a
Bohan Fang <yushian050111@gmail.com>
2026-03-13 11:13:32 +0800

    update

--- submit.sh
+++ submit.sh
@@ -9,7 +9,7 @@ mkdir -p submit
 printf "Marco Guo,jguo27\nSophia Dai, daiyefan\nBohan Fang, fang0106" > submit/team.txt
 
 # train model
-python src/NGram_Model.py train --work_dir work
+python src/NGram_Model.py train --work_dir work --n 6 --max_train 300000 --multilingual --max_per_lang 30000 --min_count 3
 
 # make predictions on example data submit it in pred.txt
 python src/NGram_Model.py test --work_dir work --test_data example/input.txt --test_output submit/pred.txt
